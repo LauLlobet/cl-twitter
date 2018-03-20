201e94a4bc84c488f37d1204440379d84e75d9f5
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-20 01:39:41 +0000

    Improve message test

--- src/test/java/com/laszlobogacsi/entity/MessageTest.java
+++ src/test/java/com/laszlobogacsi/entity/MessageTest.java
@@ -1,6 +1,5 @@
 package com.laszlobogacsi.entity;
 
-import com.laszlobogacsi.MessageFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,14 +7,14 @@ import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MessageTest {
     private LocalDateTime timeStamp;
-    private MessageFactory factory;
     private final LocalDateTime TEST_DATE_TIME = LocalDateTime.of(2018, 3, 15, 16, 20, 35); //2018-03-15 at 16:20:35
     private final ZoneId zoneId = ZoneId.systemDefault();
     private final Clock FIXED_CLOCK = Clock.fixed(TEST_DATE_TIME.atZone(zoneId).toInstant(), zoneId);
+
     @BeforeEach
     void setUp() {
         timeStamp = LocalDateTime.now(FIXED_CLOCK);
@@ -24,12 +23,11 @@ class MessageTest {
     @Test
     void can_create_message() {
         String content = "Hello World!";
-        long userId = 1;
-        Message message = new Message(1, content, userId, timeStamp);
+        String userName = "Alice";
+        Message message = new Message(content, userName, timeStamp);
 
-        assertEquals(message.getId(), 1L);
         assertEquals(message.getContent(), "Hello World!");
-        assertEquals(message.getUserId(), 1L);
+        assertEquals(message.getUserName(), "Alice");
         assertEquals(message.getCreatedAt(), LocalDateTime.parse("2018-03-15T16:20:35"));
     }
 }
