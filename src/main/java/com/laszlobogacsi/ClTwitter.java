f65801258b63924b9a7b988389330db63a46c6c3
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-03-20 01:38:33 +0000

    Added Cl Twitter the main API

--- src/main/java/com/laszlobogacsi/ClTwitter.java
+++ src/main/java/com/laszlobogacsi/ClTwitter.java
@@ -1,12 +1,70 @@
 package com.laszlobogacsi;
 
 import com.laszlobogacsi.entity.Message;
+import com.laszlobogacsi.entity.MessageFactory;
 import com.laszlobogacsi.entity.User;
+import com.laszlobogacsi.provider.ClockProvider;
+import com.laszlobogacsi.service.DatabaseService;
 
+import java.util.List;
+
+/**
+ * ClTwitter holds the public api of the program implementing all the features
+ */
 public class ClTwitter {
+    private final DatabaseService dbService; // Reference for the database service
+    private final ClockProvider clockProvider = ClockProvider.getInstance(); // reference for a clockprovider
+    private final MessageFactory messageFactory = new MessageFactory(clockProvider.getClock()); // factory class for messages
 
-    public boolean post(User user, Message message) {
-        return true;
+    /**
+     * Constructor with injected database service
+     * @param dbService instance that delegates towards other databases
+     */
+    public ClTwitter(DatabaseService dbService) {
+        this.dbService = dbService;
     }
 
+    /**
+     * Post feature, to post a message on a users timeline
+     * If user doesn't exist will create on first post.
+     * @param userName String name of the user
+     * @param messageContent String message to post on timeline
+     */
+    public void post(String userName, String messageContent) {
+        Message message = messageFactory.createMessage(messageContent, userName);
+        if(!dbService.isExist(userName)) {
+            dbService.save(new User(userName), message);
+        } else {
+            dbService.save(dbService.getUserByName(userName), message);
+        }
+    }
+
+    /**
+     * Read feature, read all the message for the given user name
+     * @param userName String user name
+     * @return List of messages for a given user, empty list if no messages present
+     */
+    public List<Message> read(String userName) {
+        return dbService.readAllMessagesFor(userName);
+    }
+
+    /**
+     * Follow feature, so one user can follow another user
+     * @param userName String user name that wants to follow someone else
+     * @param anotherUserName String user name, the on ewho will be followed
+     * @return boolean true if follow is successful, false otherwise
+     */
+    public boolean follow(String userName, String anotherUserName) {
+        return dbService.setFollow(userName, anotherUserName);
+    }
+
+    /**
+     * Wall feature, to list all the messages for a given user from the user itself and all the other followed users
+     * messages as well
+     * @param userName String user name
+     * @return a list of messages that are on the wall of a given user, including the followed users total messages
+     */
+    public List<Message> wall(String userName) {
+        return dbService.getWallFor(userName);
+    }
 }
--- src/test/java/com/laszlobogacsi/ClTwitterTest.java
+++ src/test/java/com/laszlobogacsi/ClTwitterTest.java
@@ -1,27 +1,63 @@
 package com.laszlobogacsi;
 
 import com.laszlobogacsi.entity.Message;
-import com.laszlobogacsi.entity.User;
+import com.laszlobogacsi.service.DatabaseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 class ClTwitterTest {
     private ClTwitter twitter;
-    private User mockUser;
     private Message mockMessage;
+    private DatabaseService db;
 
     @BeforeEach
     void setUp() {
-        twitter = new ClTwitter();
-        mockUser = mock(User.class);
+        db = mock(DatabaseService.class);
+        twitter = new ClTwitter(db);
         mockMessage = mock(Message.class);
     }
 
     @Test
     void can_post_a_message() {
-        assertTrue(twitter.post(mockUser, mockMessage));
+        twitter.post("Bob", "Hello World!");
+        verify(db, times(1)).save(any(), any());
+    }
+
+    @Test
+    void can_read_users_timeline_empty_by_default() {
+        List<Message> expected = new ArrayList<>();
+        List<Message> response = twitter.read("Bob");
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void can_read_users_timeline() {
+        List<Message> expected = Arrays.asList(mockMessage);
+        given(db.readAllMessagesFor(any())).willReturn(expected);
+        assertEquals(expected, twitter.read("Bob"));
+    }
+
+    @Test
+    void can_follow_another_user() {
+        given(db.setFollow(any(), any())).willReturn(true);
+        assertTrue(twitter.follow("Alice", "Bob"));
+    }
+
+    @Test
+    void can_read_users_wall() {
+        List<Message> expected = Arrays.asList(mockMessage, mockMessage);
+        given(db.getWallFor(any())).willReturn(expected);
+        assertEquals(twitter.wall("Alice"), expected);
+
     }
 }
